9ebedd3734176ac62af776d6fd819d69b03df686
Иван Евдокимов <vanechkaggmaster@gmail.com>
2026-04-13 12:42:53 +0300

    BaseEntity создан и закоммичен

---
+++ src/main/java/ru/ifmo/project/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package ru.ifmo.project.model;
+
+public abstract class BaseEntity {
+    protected Long id;
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
