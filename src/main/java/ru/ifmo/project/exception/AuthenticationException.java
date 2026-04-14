e625fe6c74394c922d3592e16fa17d3dc22ea9fa
Иван Евдокимов <vanechkaggmaster@gmail.com>
2026-04-14 11:55:01 +0300

    Исключения созданы и закоммичены

---
+++ src/main/java/ru/ifmo/project/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package ru.ifmo.project.exception;
+
+public class AuthenticationException extends Exception {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
