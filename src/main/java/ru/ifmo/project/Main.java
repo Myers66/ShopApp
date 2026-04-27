bf3d1ef669437d618e4203fcc0319355c2b414c8
Иван Евдокимов <vanechkaggmaster@gmail.com>
2026-04-27 11:12:21 +0300

    Создан Main.java, первичный запуск и тестирование

---
+++ src/main/java/ru/ifmo/project/Main.java
@@ -0,0 +1,10 @@
+package ru.ifmo.project;
+
+import ru.ifmo.project.console.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.start();
+    }
+}
