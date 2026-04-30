2208ffaaae3753363c61eda7d18dfcab6131be20
Иван Евдокимов <vanechkaggmaster@gmail.com>
2026-04-30 12:50:41 +0300

    Запуск веб-сервера параллельно с консолью в Main

--- src/main/java/ru/ifmo/project/Main.java
+++ src/main/java/ru/ifmo/project/Main.java
@@ -1,9 +1,21 @@
 package ru.ifmo.project;
 
 import ru.ifmo.project.console.Menu;
+import ru.ifmo.project.web.WebServer;
 
 public class Main {
     public static void main(String[] args) {
+        // запуск веб-сервера в отдельном потоке
+        new Thread(() -> {
+            try {
+                WebServer webServer = new WebServer(8080);
+                webServer.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        // запуск консольного меню
         Menu menu = new Menu();
         menu.start();
     }
